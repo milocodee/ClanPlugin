e77e788c7465ed531722d8a6aeeb63beae528606
Milo <milo@Mac-mini-von-Milo.local>
2025-11-10 20:10:22 +0100

    Initial commit

---
+++ src/main/kotlin/net/milocodee/clans/ClanPlugin.kt
@@ -0,0 +1,14 @@
+package net.milocodee.clans
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ClanPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
